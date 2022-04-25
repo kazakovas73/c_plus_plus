9eb5aa5a143d21bbd7c44b2d8818783988d07327
Alexander Kazakov <kazakovas7301@gmail.com>
2022-04-25 14:10:38 +0300

    main deleted unnecessary

--- RTI/RTI_project/bin/main.cpp
+++ RTI/RTI_project/bin/main.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
 #include <string>
-#include <vector>
-#include <filesystem>
 #include "parser.cpp"
 
 int main()
 {
-
     std::string filepath = "input_data/test.xml";
-
     Parser parser(filepath);
-
     parser.parse();
-
     
     return 0;
 }
